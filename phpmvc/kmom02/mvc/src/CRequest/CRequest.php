3c282267797e99caeb9c36d5368edfa85542f195
fh222dt <fh222dt@student.lnu.se>
2014-03-07 20:21:59 +0100

    städat lite

--- phpmvc/kmom02/mvc/src/CRequest/CRequest.php
+++ phpmvc/kmom02/mvc/src/CRequest/CRequest.php
@@ -34,36 +34,7 @@ class CRequest {
    * Calculates the base_url of the installation. Stores all useful details in $this.
    *
    * @param $baseUrl string use this as a hardcoded baseurl.
-   */
-  /*public function Init($baseUrl = null) {
-    // Take current url and divide it in controller, method and arguments
-    $requestUri = $_SERVER['REQUEST_URI'];
-    $scriptName = $_SERVER['SCRIPT_NAME']; 
-    $query = substr($_SERVER['REQUEST_URI'], strlen(rtrim(dirname($_SERVER['SCRIPT_NAME']), '/')));
-    $splits = explode('/', trim($query, '/'));
-    
-    // Set controller, method and arguments
-    $controller =  !empty($splits[0]) ? $splits[0] : 'index';
-    $method       =  !empty($splits[1]) ? $splits[1] : 'index';
-    $arguments = $splits;
-    unset($arguments[0], $arguments[1]); // remove controller & method part from argument list
-
-    // Prepare to create current_url and base_url
-      $currentUrl = $this->GetCurrentUrl();
-      $parts        = parse_url($currentUrl);
-      $baseUrl       = !empty($baseUrl) ? $baseUrl : "{$parts['scheme']}://{$parts['host']}" . (isset($parts['port']) ? ":{$parts['port']}" : '') . rtrim(dirname($scriptName), '/');
-   
-    // Store it
-    $this->base_url      = rtrim($baseUrl, '/') . '/';
-    $this->current_url  = $currentUrl;
-    $this->request_uri  = $_SERVER['REQUEST_URI'];
-    $this->script_name  = $_SERVER['SCRIPT_NAME'];
-    $this->query         = $query;
-    $this->splits         = $splits;
-    $this->controller     = $controller;
-    $this->method         = $method;
-    $this->arguments    = $arguments;
-  }*/
+   */  
   public function Init($baseUrl = null) {
     // Take current url and divide it in controller, method and arguments
     $requestUri = $_SERVER['REQUEST_URI'];
