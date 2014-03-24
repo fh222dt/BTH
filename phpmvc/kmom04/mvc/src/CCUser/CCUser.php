252bc3d63565f0c84bb37c6d476631d49b3a8374
fh222dt <fh222dt@student.lnu.se>
2014-03-24 10:48:45 +0100

    kmom04 tut del 7 & 8

--- phpmvc/kmom04/mvc/src/CCUser/CCUser.php
+++ phpmvc/kmom04/mvc/src/CCUser/CCUser.php
@@ -6,7 +6,7 @@
 */
 class CCUser extends CObject implements IController {
 
-  private $userModel;
+  //private $userModel;
  
 
   /**
@@ -24,61 +24,74 @@ class CCUser extends CObject implements IController {
   public function Index() {
     $this->views->SetTitle('User Controller');
     $this->views->AddInclude(__DIR__ . '/index.tpl.php', array(
-      'is_authenticated'=>$this->user->IsAuthenticated(),
-      'user'=>$this->user->GetProfile(),
+      'is_authenticated'=>$this->user['isAuthenticated'],
+                  'user'=>$this->user,
       //'user'=>$this->userModel->GetUserProfile(),
     ));
   }
 
   /**
-  * View and edit user profile.
-  */
-  public function Profile() {
-    $this->views->SetTitle('User Profile');
-    $this->views->AddInclude(__DIR__ . '/profile.tpl.php', array(
-      'is_authenticated'=>$this->user->IsAuthenticated(),
-      'user'=>$this->user->GetProfile(),
-    ));
-  }
- 
-
-  /**
-   * Authenticate and login a user.
-   */
-  /*public function Login($akronymOrEmail=null, $password=null) {
-    if($akronymOrEmail && $password) {
-      $this->user->Login($akronymOrEmail, $password);
-      $this->RedirectToController('profile');
-    }
-    $this->views->SetTitle('Login');
-    $this->views->AddInclude(__DIR__ . '/login.tpl.php');
-  }*/
-
-  /**
    * Authenticate and login a user.
    */
   public function Login() {
-    $form = new CForm();
-    $form->AddElement('acronym', array('label'=>'Acronym or email:', 'type'=>'text'));
-    $form->AddElement('password', array('label'=>'Password:', 'type'=>'password'));
-    $form->AddElement('doLogin', array('value'=>'Login', 'type'=>'submit', 'callback'=>array($this, 'DoLogin')));
+    $form = new CFormUserLogin($this);
     $form->CheckIfSubmitted();
-
     $this->views->SetTitle('Login');
-    $this->views->AddInclude(__DIR__ . '/login.tpl.php', array('login_form'=>$form->GetHTML()));     
-  }
+    $this->views->AddInclude(__DIR__ . '/login.tpl.php', 
+      array('login_form'=>$form->GetHTML())); 
+              }
 
   /**
    * Perform a login of the user as callback on a submitted form.
    */
   public function DoLogin($form) {
-    if($this->user->Login($form->GetValue('acronym'), $form->GetValue('password'))) {
+    if($this->user->Login($form['acronym']['value'], $form['password']['value'])) {
+      $this->AddMessage('success', "Welcome {$this->user['name']}.");
       $this->RedirectToController('profile');
     } else {
-      $this->RedirectToController('login');     
+      $this->AddMessage('notice', "Failed to login, user does not exist or password does not match.");
+      $this->RedirectToController('login');
     }
   }
  
+  /**
+   * View and edit user profile.
+   */
+  public function Profile() {   
+    $form = new CFormUserProfile($this, $this->user);
+    $form->CheckIfSubmitted();
+
+    $this->views->SetTitle('User Profile')
+                ->AddInclude(__DIR__ . '/profile.tpl.php', array(
+                  'is_authenticated'=>$this->user['isAuthenticated'],
+                  'user'=>$this->user,
+                  'profile_form'=>$form->GetHTML(),
+                ));
+  }
+
+  /**
+   * Change the password.
+   */
+  public function DoChangePassword($form) {
+    if($form['password']['value'] != $form['password1']['value'] || empty($form['password']['value']) || empty($form['password1']['value'])) {
+      $this->AddMessage('error', 'Password does not match or is empty.');
+    } else {
+      $ret = $this->user->ChangePassword($form['password']['value']);
+      $this->AddMessage($ret, 'Saved new password.', 'Failed updating password.');
+    }
+    $this->RedirectToController('profile');
+  }
+
+  /**
+   * Save updates to profile information.
+   */
+  public function DoProfileSave($form) {
+    $this->user['name'] = $form['name']['value'];
+    $this->user['email'] = $form['email']['value'];
+    $ret = $this->user->Save();
+    $this->AddMessage($ret, 'Saved profile.', 'Failed saving profile.');
+    $this->RedirectToController('profile');
+  }
 
   /**
    * Logout a user.
