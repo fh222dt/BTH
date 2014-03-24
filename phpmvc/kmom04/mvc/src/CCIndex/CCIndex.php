562f6389be9ce82158994c8a821594ecf2375433
fh222dt <fh222dt@student.lnu.se>
2014-03-24 08:33:08 +0100

    kmom04 tut del 5

--- phpmvc/kmom04/mvc/src/CCIndex/CCIndex.php
+++ phpmvc/kmom04/mvc/src/CCIndex/CCIndex.php
@@ -1,18 +1,46 @@
-    <?php
-    /**
-    * Standard controller layout.
-    *
-    * @package WoolyCore
-    */
-    class CCIndex implements IController {
+<?php
+/**
+* Standard controller layout.
+*
+* @package WoolyCore
+*/
+class CCIndex extends CObject implements IController {
 
-       /**
-        * Implementing interface IController. All controllers must have an index action.
-        */
-       public function Index() {   
-          global $wo;
-          $wo->data['title'] = "The Index Controller";
-          $wo->data['main'] = "<h1>The Index Controller</h1>";
-       }
+  /**
+  * Constructor
+  */
+  public function __construct() {
+    parent::__construct();
+  }
 
-    } 
+  /**
+  * Implementing interface IController. All controllers must have an index action.
+  */
+  public function Index() {
+    $this->views->SetTitle('Index Controller');
+    $this->views->AddInclude(__DIR__ . '/index.tpl.php', array('menu'=>$this->Menu()));
+  }
+
+  /**
+   * A menu that shows all available controllers/methods
+   */
+  private function Menu() {   
+    $items = array();
+    foreach($this->config['controllers'] as $key => $val) {
+      if($val['enabled']) {
+        $rc = new ReflectionClass($val['class']);
+        $items[] = $key;
+        $methods = $rc->getMethods(ReflectionMethod::IS_PUBLIC);
+        foreach($methods as $method) {
+          if($method->name != '__construct' && $method->name != '__destruct' && $method->name != 'Index') {
+            $items[] = "$key/" . mb_strtolower($method->name);
+          }
+        }
+      }
+    }
+    return $items;
+  }
+
+
+
+} 
